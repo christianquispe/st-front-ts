711021fabb96c97aa9ce282da56ddb92a42acb1c
christianquispe <christianquispecamasca@gmail.com>
2021-12-22 21:39:44 -0500

    chore: implement schemas for queries

---
+++ src/graphql/resolvers/index.tsx
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+
+export const ALL_PICTURE_QUERY = gql`
+  query allPictures($paginated: Paginated, $filter: FilterPicture) {
+    allPictures(paginated: $paginated, filter: $filter) {
+      pageInfo {
+        total
+        currentPage
+        perPage
+        hasPreviousPage
+        hasNextPage
+      }
+      pictures {
+        _id
+        name
+        description
+        status
+        price
+        simbol
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
+
+export const GET_PICTURE = gql`
+  query getPicture($id: ID!) {
+    getPicture(input: { id: $id }) {
+      _id
+      name
+      description
+      status
+    }
+  }
+`;
