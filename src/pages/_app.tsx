e3d57542337155902cfb8588e36e841e56d12180
christianquispe <christianquispecamasca@gmail.com>
2021-12-22 21:38:01 -0500

    chore: setup apollo on the preject

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apollo";
 
 import "../styles/index.css";
 import "../styles/normalize.css";
@@ -6,5 +8,11 @@ import "../styles/vars.css";
 import "../styles/common.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
